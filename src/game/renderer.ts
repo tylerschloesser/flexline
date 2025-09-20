c7da9e11cb608b08bfd8bc898faf3d7872407ad3
Tyler Schloesser <tylerschloesser@gmail.com>
2025-09-20 10:13:56 -0700

    fix lint

--- src/game/renderer.ts
+++ src/game/renderer.ts
@@ -181,8 +181,7 @@ export class GameRenderer {
       }
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    this.viewport.on("clicked", (event: any) => {
+    this.viewport.on("clicked", (event: PIXI.FederatedPointerEvent) => {
       if (!this.viewport) return;
       const worldPoint = this.viewport.toWorld(event.data.global);
       this.handleClick(worldPoint.x, worldPoint.y);
